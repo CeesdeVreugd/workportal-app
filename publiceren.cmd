3c5b70731af82b12341a6f55b9acaab9a0679c11
Cees | De Vreugd Productietechniek <cees@devreugd-pt.nl>
2026-09-26 00:20:11 +0200

    WorkPortal V15: Verkoop-Inkoop-WVB lichtblauw

--- publiceren.cmd
+++ publiceren.cmd
@@ -12,7 +12,7 @@ setlocal
 
 set "DOEL=C:\1 Automatiserings projecten\DeVreugdProductietechniek\WorkPortal-App\WorkPortal"
 set "REPO=https://github.com/CeesdeVreugd/workportal-app.git"
-set "OMSCHRIJVING=WorkPortal V14: kleuren per functierol"
+set "OMSCHRIJVING=WorkPortal V15: Verkoop-Inkoop-WVB lichtblauw"
 
 echo.
 echo ============================================
